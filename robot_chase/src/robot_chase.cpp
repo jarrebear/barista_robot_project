e7231f974cbf9b16adf5232824204fc8c845e8b9
jarrebear <battistini.jarrett@gmail.com>
2026-08-11 21:47:01 +0000

    Added in code for rick to chase morty

--- robot_chase/src/robot_chase.cpp
+++ robot_chase/src/robot_chase.cpp
@@ -1,8 +1,11 @@
 #include <chrono>
+#include <cmath>
 #include <functional>
 
 #include <geometry_msgs/msg/transform_stamped.hpp>
 #include <geometry_msgs/msg/twist.hpp>
+#include <geometry_msgs/msg/vector3.hpp>
+
 #include <rclcpp/rclcpp.hpp>
 #include <tf2_ros/buffer.h>
 #include <tf2_ros/transform_listener.h>
@@ -28,17 +31,32 @@ private:
       auto transform = tf_buffer_.lookupTransform(
           "rick/base_link", "morty/base_link", tf2::TimePointZero);
 
-      const auto &t = transform.transform.translation;
-      const auto &q = transform.transform.rotation;
+      const geometry_msgs::msg::Vector3 &t = transform.transform.translation;
 
+      double error_distance = calculate_distance(t);
+      double error_yaw = std::atan2(t.y, t.x);
+
+      auto msg = geometry_msgs::msg::Twist();
+      msg.linear.x = (error_distance - offset_distance_) * kp_distance_;
+      msg.angular.z = error_yaw * kp_yaw_;
+      publisher_->publish(msg);
       RCLCPP_INFO(this->get_logger(),
-                  "Translation: [%.3f, %.3f, %.3f], "
-                  "Rotation: [%.3f, %.3f, %.3f, %.3f]",
-                  t.x, t.y, t.z, q.x, q.y, q.z, q.w);
+                  "Publishing: linear.x=%.2f, angular.z = % .2f ", msg.linear.x,
+                  msg.angular.z);
+
     } catch (const tf2::TransformException &ex) {
       RCLCPP_WARN_THROTTLE(this->get_logger(), *this->get_clock(), 2000,
                            "Could not get transform: %s", ex.what());
     }
+  }
+
+  double
+  calculate_distance(const geometry_msgs::msg::Vector3 &translation_dist) {
+    double dx = translation_dist.x;
+    double dy = translation_dist.y;
+    double dz = translation_dist.z;
+
+    return std::sqrt(dx * dx + dy * dy + dz * dz);
   }
 
 private:
@@ -46,6 +64,13 @@ private:
   rclcpp::TimerBase::SharedPtr timer_;
   tf2_ros::Buffer tf_buffer_;
   tf2_ros::TransformListener tf_listener_;
+
+  // Rick should be more than 0.5 meters from morty
+  double offset_distance_ = 0.5;
+
+  // PID gains
+  double kp_yaw_ = 1.5;
+  double kp_distance_ = 1.5;
 };
 
 int main(int argc, char **argv) {
